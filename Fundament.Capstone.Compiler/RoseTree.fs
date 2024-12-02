7ffa5397f57e67f467d9b627df4b74379fff69b0
Izzy Lancaster <avrisaac555@gmail.com>
2024-12-01 16:42:37 -0800

    Add fromEdges and leaf to RoseTree

--- Fundament.Capstone.Compiler/RoseTree.fs
+++ Fundament.Capstone.Compiler/RoseTree.fs
@@ -7,6 +7,43 @@ type RoseTree<'T> = { Root: 'T; Children: RoseForest<'T> }
 and RoseForest<'T> = RoseTree<'T> list
 
 module RoseTree =
+
+    let leaf root = { Root = root; Children = [] }
+
+    let fromEdges (edges: Map<'a, list<'a>>) (rootNodes: 'a list) : RoseForest<'a> =
+        // Basically a DFS, but on the return step we construct the RoseTree
+        // Implemented the DFS imperatively because the optimal implementation is easier to read and write than
+        // a tail-recursive DFS.
+        let mutable stack = rootNodes
+        let mutable visited = Set.empty
+        let mutable acc = Map.empty
+
+        while not stack.IsEmpty do
+            if not (Set.contains stack.Head visited) then
+                // New node, mark it as visited
+                visited <- Set.add stack.Head visited
+
+                // If it has children, add it to the stack
+                match Map.tryFind stack.Head edges with
+                | Some children -> stack <- children @ stack
+                | None -> ()
+            else
+                // We're returning to this node after previously visiting, meaning all children (if any) have been visited and processed.
+                let children =
+                    edges
+                    |> Map.tryFind stack.Head
+                    |> Option.defaultValue []
+                    |> List.map (fun child -> Map.find child acc)
+
+                let node =
+                    { Root = stack.Head
+                      Children = children }
+
+                acc <- Map.add stack.Head node acc
+                stack <- stack.Tail
+
+        rootNodes |> List.map (fun root -> Map.find root acc)
+
     let rec foldTree<'T, 'State> (folder: 'T -> 'State list -> 'State) (tree: RoseTree<'T>) =
         match tree with
         | { Root = root; Children = [] } -> folder root []
